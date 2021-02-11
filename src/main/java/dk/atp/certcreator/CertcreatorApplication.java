9fcb941f467029fbf544cc13ac71472909373204
unknown <x_system16@hotmail.com>
2021-02-11 03:27:53 +0100

    Test

---
+++ src/main/java/dk/atp/certcreator/CertcreatorApplication.java
@@ -0,0 +1,13 @@
+package dk.atp.certcreator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertcreatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CertcreatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dk/atp/certcreator/CertcreatorApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.atp.certcreator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertcreatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
